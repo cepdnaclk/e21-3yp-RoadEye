98bc7c406ae432205f2c99851f5b5f79fddc8434
cielo019 <e21019@eng.pdn.ac.lk>
2026-04-12 19:30:39 +0530

    Fix the issues

--- RoadEye/src/App.jsx
+++ RoadEye/src/App.jsx
@@ -1,9 +1,10 @@
 import { NavigationContainer } from '@react-navigation/native'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { AuthProvider, useAuth } from './hooks/useAuth'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
+import LoginPage     from './pages/LoginPage'
+import SignupPage    from './pages/SignupPage'
 import DashboardPage from './pages/DashboardPage'
+import EmergencyPage from './pages/EmergencyPage'
 
 const Stack = createNativeStackNavigator()
 
@@ -12,11 +13,14 @@ function RootNavigator() {
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       {isLoggedIn ? (
-        <Stack.Screen name="Dashboard" component={DashboardPage} />
+        <>
+          <Stack.Screen name="Dashboard"  component={DashboardPage} />
+          <Stack.Screen name="Emergency"  component={EmergencyPage} />
+        </>
       ) : (
         <>
-          <Stack.Screen name="Login" component={LoginPage} />
-          <Stack.Screen name="Signup" component={SignupPage} />
+          <Stack.Screen name="Login"   component={LoginPage} />
+          <Stack.Screen name="Signup"  component={SignupPage} />
         </>
       )}
     </Stack.Navigator>
